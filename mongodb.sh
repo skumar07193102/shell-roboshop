d4c669254fff708ba14dc67dea804c0ed326e60a
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-28 19:09:40 +0530

    some

---
+++ mongodb.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0269c42f363b9217b"
+Hosted_zone_ID="Z02845132921NLJI9M7EG"
+Domain="devsecops86s.space"
+instance_name=$1
+instance_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" --query 'Instances[0].InstanceId' --output text)
+instance_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+record_name=$instance_name.$Domain
+echo "successfully created $instance_name server : $instance_ip"
+sudo cp mongo.repo /etc/yum.repos.d/
+if [ $? -eq 0 ]; then
+    dnf install mongodb-org -y
+fi
+systemctl enable mongod
+systemctl start mongod 
+
+
+
