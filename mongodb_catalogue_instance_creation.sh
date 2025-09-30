ccb6ef9a4de23e4ea3e47d15a8eaff7551a39e75
sunil.kumar <sunil.kumar07193102@gmail.com>
2025-09-30 20:45:17 +0530

    some

--- mongodb_catalogue_instance_creation.sh
+++ mongodb_catalogue_instance_creation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-AMI_ID="RHEL-9-DevOps-Practice"
+AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0d7af0cacff646e77"
 instance_type="t3.micro"
 host_zone_id="Z02845132921NLJI9M7EG"
@@ -7,7 +7,7 @@ Domain=devsecops86s.space
 for instance in $@
 do
      instance_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-     
+
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         servername=$instance
